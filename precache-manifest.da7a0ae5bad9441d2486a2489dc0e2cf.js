663e0d50ee11761d61b837a9cb59026829f8c75c
Derrick Strong <derrickmstrong@gmail.com>
2020-08-20 20:27:30 -0500

    Updates

---
+++ precache-manifest.da7a0ae5bad9441d2486a2489dc0e2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b659a8214e6e859a3cd6c3cdb56d3c5b",
+    "url": "/project-reacttodo/index.html"
+  },
+  {
+    "revision": "ff46b9423a64fdf7f80f",
+    "url": "/project-reacttodo/static/css/main.c1f5fe0c.chunk.css"
+  },
+  {
+    "revision": "3168a05617e1cef86355",
+    "url": "/project-reacttodo/static/js/2.2386faeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project-reacttodo/static/js/2.2386faeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff46b9423a64fdf7f80f",
+    "url": "/project-reacttodo/static/js/main.3931e658.chunk.js"
+  },
+  {
+    "revision": "56120cfb53f93bb3c5b7",
+    "url": "/project-reacttodo/static/js/runtime-main.432b9e49.js"
+  }
+]);
